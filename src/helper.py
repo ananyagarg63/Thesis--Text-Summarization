26414c4694390991ff15b5c50459abf9a305aa21
Ananya Garg <ananyagarg63@gmail.com>
2017-11-21 16:56:27 +0530

    mapping

--- src/helper.py
+++ src/helper.py
@@ -95,5 +95,19 @@ def pairs():
         frequency.append(words.count(i))
     print(list(zip(words, frequency)))
 
+uniques = []
+text = input("Enter text")
+words = text.split(' ')
+for each in words:
+    if each not in uniques:
+        uniques.append(each)
+print(uniques)
+
+char_to_int = dict((c, i) for i, c in enumerate(words))
+print(char_to_int)
+
+int_to_char = dict((i, c) for i, c in enumerate(words))
+print(int_to_char)
+
 
 
